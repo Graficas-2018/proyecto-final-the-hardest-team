0e7edb5fa74e00e198d699a07e5b07cd2e2ef213
Luigi-ITESM <a01129926@itesm.mx>
2018-11-28 22:09:26 -0600

    Add background sound

--- Hardest-Game/Hardest_Game.js
+++ Hardest-Game/Hardest_Game.js
@@ -34,7 +34,7 @@ var enemy;
 
 var mainCharBox;
 
-var whichLevel = 3;
+var whichLevel = 1;
 
 function createMap(theLevel) {
 
@@ -542,6 +542,22 @@ function createScene(canvas) {
     
     // Create the objects
     //loadFBX();
+
+    // Load global audio
+    var listener = new THREE.AudioListener();
+    camera.add( listener );
+
+    // create a global audio source
+    var sound = new THREE.Audio( listener );
+
+    // load a sound and set it as the Audio object's buffer
+    var audioLoader = new THREE.AudioLoader();
+    audioLoader.load( '../audio/mainTheme.mp3', function( buffer ) {
+        sound.setBuffer( buffer );
+        sound.setLoop( true );
+        sound.setVolume( 0.5 );
+        sound.play();
+    });
 
 
 
